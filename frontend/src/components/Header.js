63c39dc53b6f8d1d2f5bbaa0dda1cd7f22f3c7b0
moha09-coder <mohamedelmossalamy@gmail.com>
2020-11-25 13:03:10 +0200

    Implemented React Router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const header = () => {
@@ -6,16 +7,22 @@ const header = () => {
     <header>
       <Navbar bg='light' variant='light' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>Proshop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Proshop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart'></i> Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user'></i> Sign in
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Sign in
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
