4d76c2613b48ddb22cc902b709dc66b9e36ef60c
moha09-coder <mohamedelmossalamy@gmail.com>
2020-12-01 15:13:56 +0200

    Introduced shopping cart with adding and removing products from it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,8 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
-import Productcreen from './screens/ProductScreen'
+import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 const App = () => {
   return (
@@ -13,7 +14,8 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Route path='/' component={HomeScreen} exact />
-          <Route path='/product/:id' component={Productcreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer />
